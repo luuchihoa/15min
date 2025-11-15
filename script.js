97e882518391232c411afb937b88205adfd78db3
Luu Chi Hoa <91874475+luuchihoa@users.noreply.github.com>
2025-11-15 23:40:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ====================== ÂM THANH =========================
-const hoverSound = new Audio('sound/hover.mp3');
-const selectSound = new Audio('sound/select.mp3');
-const winSound = new Audio('sound/win.mp3');
+const hoverSound = new Audio('/hover.mp3');
+const selectSound = new Audio('/select.mp3');
+const winSound = new Audio('/win.mp3');
 winSound.volume = 0.35;
 hoverSound.volume = 1;
 selectSound.volume = 1;
